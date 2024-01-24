f70db2516c3ece7a67e5c7464a571e8a643ca288
Bukach Vlad <vladbuckach@ukr.net>
2024-01-24 11:24:12 +0200

    Initial commit

---
+++ src/main/java/com/example/mangatranslator/MangaTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangatranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaTranslatorApplication.class, args);
+    }
+
+}
