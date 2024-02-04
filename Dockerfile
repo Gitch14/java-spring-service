652b1efb20c85fb97790de2b6d9394d2eb5c6a43
Bukach Vlad <vladislav.bukach@geeksforless.com>
2024-02-04 23:25:36 +0200

    hot fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM ubuntu:latest
-LABEL authors="vladb"
+FROM openjdk:17
 
-ENTRYPOINT ["top", "-b"]
+ARG JAR_FILE=target/java-spring-service-0.0.1-SNAPSHOT.jar
+
+WORKDIR /opt/app
+
+COPY ${JAR_FILE} app.jar
+
+EXPOSE 8081
+
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/example/mangatranslator/controller/MainController.java
+++ src/main/java/com/example/mangatranslator/controller/MainController.java
@@ -1,4 +1,14 @@
 package com.example.mangatranslator.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "Hello World!";
+    }
+
 }
