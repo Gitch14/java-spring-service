9a34c1c39c61560f5b959763f9889a01489bd2c5
Bukach Vlad <vladbuckach@ukr.net>
2024-02-04 23:10:07 +0200

    Revert "Detached2 (#1)"
    This reverts commit 7b6fb1d06b3397493c2e91510af80e0edee234d1.

--- src/main/java/com/example/mangatranslator/controller/MainController.java
+++ src/main/java/com/example/mangatranslator/controller/MainController.java
@@ -1,15 +1,4 @@
 package com.example.mangatranslator.controller;
 
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
 public class MainController {
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
-    }
 }
