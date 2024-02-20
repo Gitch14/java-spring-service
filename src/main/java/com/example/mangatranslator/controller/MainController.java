f2da380d135b7e9b3a2ac42bc40d9b7dba446511
Sergey Khomich <sergii.khomych@geeksforless.com>
2024-02-20 10:58:54 +0200

    test

--- src/main/java/com/example/mangatranslator/controller/MainController.java
+++ src/main/java/com/example/mangatranslator/controller/MainController.java
@@ -1,29 +1,25 @@
 package com.example.mangatranslator.controller;
 
-import com.example.mangatranslator.model.Recipe;
+import com.example.mangatranslator.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Autowired
+    private MyService myService;
+
     @GetMapping("/")
     public String mainPage() {
+        myService.someMethodForNew();
         return "index";
     }
 
     @GetMapping("/createNew")
     public String mainPageS() {
         return "new-recipe";
-    }
-
-    @PostMapping("/saveNew")
-    public Recipe mainPageP() {
-        //Service service = new Service();
-        //service.new();
-        Recipe recipe = new Recipe();
-        return recipe;
     }
 
 }
