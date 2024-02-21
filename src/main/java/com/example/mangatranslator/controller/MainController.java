e2ebb1410507336c0c5a57479d8bc06dc6620996
Sergey Khomich <sergii.khomych@geeksforless.com>
2024-02-21 19:36:33 +0200

    update docker-compose and small test

--- src/main/java/com/example/mangatranslator/controller/MainController.java
+++ src/main/java/com/example/mangatranslator/controller/MainController.java
@@ -1,20 +1,29 @@
 package com.example.mangatranslator.controller;
 
+import com.example.mangatranslator.model.User1;
+import com.example.mangatranslator.repository.User1Repository;
 import com.example.mangatranslator.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MainController {
 
     @Autowired
     private MyService myService;
+    @Autowired
+    private User1Repository repository;
 
     @GetMapping("/")
-    public String mainPage() {
+    public List<User1> mainPage() {
+        List<User1> list = new ArrayList<>();
+        list = repository.findAll();
       //  myService.someMethodForNew();
-        return "index";
+        return list;
     }
 
     @GetMapping("/createNew")
