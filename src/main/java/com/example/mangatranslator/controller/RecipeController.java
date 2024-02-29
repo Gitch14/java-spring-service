06f0e51bd14c5cd57476b2dc339a77570ee7cfca
Sergey Khomich <sergii.khomych@geeksforless.com>
2024-02-29 12:28:07 +0200

    add controller for recipe

---
+++ src/main/java/com/example/mangatranslator/controller/RecipeController.java
@@ -0,0 +1,59 @@
+package com.example.mangatranslator.controller;
+
+import com.example.mangatranslator.model.Recipe;
+import com.example.mangatranslator.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import static com.example.mangatranslator.util.Const.*;
+
+@Controller
+public class RecipeController {
+    @Autowired
+    private RecipeService recipeService;
+    @GetMapping(CATALOG_PAGE)
+    String catalog(Model model) {
+        model.addAttribute("recipe",recipeService.listRecipe());
+        return "catalog";
+    }
+
+    @GetMapping(RECIPE_PAGE)
+    String recipe(Model model,@PathVariable(value = "id") long id) {
+        model.addAttribute("recipe",recipeService.getRecipeById(id));
+        return "recipe-page";
+    }
+
+    @GetMapping(CREATE_RECIPE_PAGE)
+    String recipeCreatePage() {
+
+        return "recipe-create";
+    }
+
+    @PostMapping(CREATE_RECIPE)
+    public String createRecipe(Recipe recipe) {
+
+        return "redirect:/my/posts";
+    }
+
+    @GetMapping(UPDATE_RECIPE_PAGE)
+    String recipeUpdatePage() {
+
+        return "recipe-create";
+    }
+
+    @PostMapping(UPDATE_RECIPE)
+    public String updateRecipe(Recipe recipe) {
+
+        return "redirect:/my/posts";
+    }
+
+    @PostMapping(DELETE_RECIPE)
+    public String deleteRecipe(Recipe recipe) {
+
+        return "redirect:/my/posts";
+    }
+}
--- src/main/java/com/example/mangatranslator/util/Const.java
+++ src/main/java/com/example/mangatranslator/util/Const.java
@@ -2,13 +2,21 @@ package com.example.mangatranslator.util;
 
 public class Const {
     //GET mapping
-    public static final String CREATE_RECIPE = "recipe-service.create";
-    public static final String UPDATE_RECIPE = "recipe-service.update";
-    public static final String DELETE_RECIPE = "recipe-service.remove";
+    public static final String CREATE_RECIPE_PAGE = "/create/recipe";
+    public static final String UPDATE_RECIPE_PAGE = "/update/recipe";
     public static final String REGISTRATION_PAGE = "/registration";
     public static final String LOGIN_PAGE = "/login";
     public static final String HOME_PAGE = "/";
 
+    public static final String CATALOG_PAGE = "/catalog";
+
+    public static final String RECIPE_PAGE = "/recipe/{id}";
+
     //POST mapping
     public static final String CREATE_USER = "/register";
+    public static final String CREATE_RECIPE = "/recipe/create";
+
+    public static final String UPDATE_RECIPE = "recipe/update";
+
+    public static final String DELETE_RECIPE = "recipe/delete";
 }
